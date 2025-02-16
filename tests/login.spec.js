6e1f98f671f7f0e883f8b418e5771a23a4011efa
Thaynara <loboo.thaynara@gmail.com>
2025-02-15 22:31:16 -0300

    Integração com banco de dados e page objects

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,23 +1,42 @@
 import { test, expect } from '@playwright/test';
+import { obterCodigo2FA } from '../support/db';
+import { LoginPage } from '../pages/LoginPage';
+import { DashPage } from '../pages/DashPage';
 
-test('Nao deve logar quando o codigo de autenticacao e valido', async ({ page }) => {
-  
+test('Não deve logar quando o código de autentição é inválido', async ({ page }) => {
+  const loginPage = new LoginPage(page)
   const usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
   
-  await page.goto('http://paybank-mf-auth:3000/');
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for(const digito of usuario.senha){
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCPF(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+  await loginPage.informa2FA('123456')
 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+  
+  await loginPage.acessaPagina()
+  await loginPage.informaCPF(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+
+  //temporário
+  await page.waitForTimeout(3000)
+
+  const code = await obterCodigo2FA()
+  await loginPage.informa2FA(code)
+
+  await page.waitForTimeout(2000)
+
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
