791bec38b474c319c6c8fdfb0d15ebca1848a493
Thaynara <loboo.thaynara@gmail.com>
2025-02-18 11:24:58 -0300

    Consumindo filas do redis

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
+import { cleanJobs, getJob } from '../support/redis';
 
 test('Não deve logar quando o código de autentição é inválido', async ({ page }) => {
   const loginPage = new LoginPage(page)
@@ -26,17 +27,19 @@ test('Deve acessar a conta do usuário', async ({ page }) => {
     senha: '147258'
   }
   
+  await cleanJobs()
+
   await loginPage.acessaPagina()
   await loginPage.informaCPF(usuario.cpf)
   await loginPage.informaSenha(usuario.senha)
+  //checkpoint
+  await page.getByRole('heading', {name: 'Verificaçao em duas etepas'}).waitFor({timeout: 3000})
 
-  //temporário
-  await page.waitForTimeout(3000)
+  //consumindo do redis
+  const code = await getJob()
 
-  const code = await obterCodigo2FA()
+  //const code = await obterCodigo2FA(usuario.cpf)
   await loginPage.informa2FA(code)
 
-  await page.waitForTimeout(2000)
-
-  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+ await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
